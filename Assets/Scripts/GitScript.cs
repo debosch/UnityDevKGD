91039aeb76a0a8efb877eaaf5e57c500ac2c0716
Valeriy <mr.robariii@gmail.com>
2020-09-21 20:24:35 +0200

    Created Scenes and Scripts folders
    Added scene
    Added script

---
+++ Assets/Scripts/GitScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GitScript : MonoBehaviour
+{
+    public GameObject simpleGO;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
