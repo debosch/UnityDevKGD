629a0336dbd3b09ae091e0b921f4c7b7012fe667
BepeckoB <BepeckoB@gmail.com>
2020-09-28 20:43:07 +0200

    Managers folder added

---
+++ Assets/Scripts/Managers/UpdateManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game
+{
+    public class UpdateManager : MonoBehaviour
+    {
+        [SerializeField]
+        private ScriptableEvent _updateEvent;
+
+        [SerializeField]
+        private ScriptableEvent _fixedEvent;
+
+        private void Update()
+        {
+            _updateEvent.Dispatch();
+        }
+
+        private void FixedUpdate()
+        {
+            _fixedEvent.Dispatch();
+        }
+    }
+}
