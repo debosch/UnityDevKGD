5a9e4275e9d442e190aca875c8b8502c2c5a138b
Oleg <oleg.kalyugin2001@mail.ru>
2020-09-28 20:42:10 +0200

    manager

---
+++ Assets/Script/managers/UpdateManagers.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Events;
+
+public class UpdateManagers : MonoBehaviour
+{
+    [SerializeField]
+    private ScriptableEvent _updateEvent;
+
+    private void Update()
+    {
+        _updateEvent.Dispatch();
+    }
+
+    private void FixedUpdate()
+    {
+        _fixedEvent.Dispatch();
+    }
+}
