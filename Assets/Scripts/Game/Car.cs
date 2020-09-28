4230542aea572916535bf7568a20d126d39f78c2
gylesis <nikitadedov66@gmail.com>
2020-09-28 20:39:06 +0300

    car class created

---
+++ Assets/Scripts/Game/Car.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Events;
+
+namespace Game
+{
+    public class Car : MonoBehaviour
+    {
+        [SerializeField]
+        private EventListeners _updaterEventListener;
+
+        private void Awake()
+        {
+            _updaterEventListener.OnEventHappened += Move;
+        }
+
+
+        private void Move()
+        {
+            Debug.Log("Move");
+        }
+    }
+
+}
