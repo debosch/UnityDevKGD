0f0c704f7b7ef6f3c63ea09c1acd01519bf40518
Nikita Shpulin <shpulinn@mail.ru>
2020-09-28 20:39:59 +0300

    event system created

---
+++ Assets/Scripts/Events/EventDispatcher.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Events {
+    public class EventDispatcher : MonoBehaviour {
+
+        [SerializeField]
+        private ScriptableEvent _someEvent;
+
+        public void Dispatch() {
+            _someEvent.Dispatch();
+        }
+    }
+
+}
+
---
+++ Assets/Scripts/Events/EventListener.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace Events {
+
+    public class EventListener : MonoBehaviour {
+
+        [SerializeField]
+        private ScriptableEvent _someEvent;
+
+        public event Action OnEventHappened = delegate { };
+
+        private void OnEnable() {
+            _someEvent.AddListener(EventHappaned);
+        }
+
+        private void OnDisable() {
+            _someEvent.RemoveListener(EventHappaned);
+        }
+
+        private void EventHappaned() {
+            OnEventHappened.Invoke();
+        }
+
+    }
+
+}
+
+
