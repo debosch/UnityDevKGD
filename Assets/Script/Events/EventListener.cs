3f0ed7ad2e94b0390ae6ac11599398a34834d8b1
Oleg <oleg.kalyugin2001@mail.ru>
2020-09-28 20:39:29 +0200

    event

---
+++ Assets/Script/Events/EventListener.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+namespace Events
+{
+    public class EventListener : MonoBehaviour
+    {
+        [SerializeField]
+        private ScriptableEvent _someEvent;
+
+        public event Action OnEventHappened = delegate { };
+
+        private void OnEnable()
+        {
+            _someEvent.AddListener(EventHappened);
+
+        }
+
+        private void OnDisable()
+        {
+            _someEvent.RemoveListener(EventHappened);
+        }
+        private void EventHappened()
+        {
+                OnEventHappened();
+        }
+
+        private void SomeMethod()
+        {
+            //logic
+            Debug.Log("!!!!");
+        }
+    }
+    
+    
+}
---
+++ Assets/Script/Events/ScriptableEvent.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+
+namespace Event { 
+
+   [CreateAssetMenu(fileName = "Event",menuName = "Event")]
+    public class ScriptableEvent : ScriptableObject
+    {
+
+        private List<Action> listeners;
+
+        public void AddListener (Action action) {
+            if (listeners = null) {
+                listeners = new List<Action>();
+
+            }
+
+            if(listeners.IndexOf(action) == -1) {
+                listeners.Add(action);
+            }
+        }
+
+        public void RemoveListener(Action action)
+        {
+            if (listeners == null) {
+                listeners = new List<Action>();
+                return;
+            }
+            listeners.Remove(action);
+        }
+        public void Dispatch ()
+        {
+            if (_listeners == null) {
+                return;
+            }
+            for (int i = 0; i< _listeners.Count; i++) {
+                _listeners(i);
+            }
+        }
+    }
+
+ }
+
+
