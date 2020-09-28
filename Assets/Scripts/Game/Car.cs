09a245c725b8636dcb2192b6161473239ef1337d
Nikita <Nkanunov@gmail.com>
2020-09-28 20:43:34 +0200

    car class created

---
+++ Assets/Scripts/Game/Car.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Events;
+using UnityEngine;
+
+namespace Game {
+    
+    public class Car : MonoBehaviour {
+        [SerializeField] private EventListener _updateEventListener;
+
+        private void Awake() {
+            _updateEventListener.onEventHappened += Move;
+        }
+
+        private void Move() {
+            Debug.Log("Move");
+        }
+
+    }
+}
