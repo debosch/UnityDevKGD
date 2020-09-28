97d62de96421eafdf855b10bce63b31b48fb9017
BarbadosP <var0720@gmail.com>
2020-09-28 20:39:58 +0200

    Events system created

---
+++ Assets/Scripts/Events/EventDispatcher.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Events
+{
+    public class EventDispatcher : MonoBehaviour
+    {
+        [SerializeField]
+        private ScriptableEvent _someEvent;
+
+        private void Dispatch()
+        {
+            _someEvent.Dispatch();
+        }
+    }
+
+}
