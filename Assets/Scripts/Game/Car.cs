ad644c438206f2b13796b0962ad3d8c052b2015e
Evgenii <jekaweek1@gmail.com>
2020-09-28 20:45:07 +0200

    Game Commit

---
+++ Assets/Scripts/Game/Car.cs
@@ -0,0 +1,23 @@
+﻿using Events;
+using UnityEngine;
+
+namespace Game { 
+
+    public class Car : MonoBehaviour {
+
+        [SerializeField]
+        private EventListener _updateEventListener;
+
+        private void Awake() {
+            _updateEventListener.OnEventHappened += Move;
+            _updateEventListener.OnEventHappened += Die;
+        }
+        private void Move() {
+            Debug.Log("Move");
+        }
+
+        private void Die() {
+
+        }
+    }
+}
