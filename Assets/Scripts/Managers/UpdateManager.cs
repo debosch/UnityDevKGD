599a1749bc6d2029027657cce0b46e6a32cfb39d
gylesis <nikitadedov66@gmail.com>
2020-09-28 20:38:12 +0300

    UpdateManager created

---
+++ Assets/Scripts/Managers/UpdateManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Events;
+
+namespace Game { 
+
+    public class UpdateManager : MonoBehaviour {
+
+        [SerializeField]
+        private ScriptableEvent updateEvent;
+
+
+        private void Update() {
+            updateEvent.Dispatch();
+        }
+
+    }
+}
