87a32dd38570b259303d15762f8dfc63652f3f89
Evgenii <jekaweek1@gmail.com>
2020-09-28 20:44:39 +0200

    Manager commit

---
+++ Assets/Scripts/Managers/UpdateManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Events;
+using UnityEngine;
+
+namespace Game
+{
+
+    public class UpdateManager : MonoBehaviour
+    {
+        [SerializeField]
+        private ScriptableEvent _updateEvent;
+
+        [SerializeField]
+        private ScriptableEvent _fixedEvent;
+        // Update is called once per frame
+        private void Update() {
+            _updateEvent.Dispatch();
+        }
+
+        private void FixedUpdate()
+        {
+            _fixedEvent.Dispatch();
+        }
+    }
+}
