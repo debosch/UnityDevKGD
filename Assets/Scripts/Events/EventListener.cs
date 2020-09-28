72a4362d033eae4e5ba21950962dd0a3ee395cbd
Nikita <Nkanunov@gmail.com>
2020-09-28 20:40:01 +0200

    event system created

---
+++ Assets/Scripts/Events/EventListener.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Events {
+    
+    public class EventListener : MonoBehaviour {
+        [SerializeField] private ScriptableEvent _someEvent;
+
+        public event Action onEventHappened = delegate { };
+
+        private void OnEnable() {
+            _someEvent.AddListener(EventHappened);
+        }
+
+        private void OnDisable() {
+            _someEvent.RemoveListener(EventHappened);
+        }
+
+        private void EventHappened() {
+            onEventHappened.Invoke();
+        }
+
+        // void Start() {
+        //     _someEvent.AddListener(SomeMethod);
+        // }
+
+        // private void SomeMethod() {
+        //     //logic
+        //         Debug.Log("!!!!");
+        // }
+
+        // Update is called once per frame
+        void Update() { }
+    }
+}
