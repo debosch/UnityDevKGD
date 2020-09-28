4a74e381b41bc823147854f1be529fc2c3a53897
gylesis <nikitadedov66@gmail.com>
2020-09-28 20:35:44 +0300

    event sys createad

---
+++ Assets/Scripts/Events/EventDispetcher.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace Events { 
+    
+    public class EventDispetcher : MonoBehaviour {
+
+        [SerializeField]
+        private ScriptableEvent _someEvent;
+
+        public void Dispatch() {
+            _someEvent.Dispatch();
+        }
+    }
+}
+
---
+++ Assets/Scripts/Events/EventListeners.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+
+namespace Events {
+    public class EventListeners : MonoBehaviour {
+
+        [SerializeField]
+        private ScriptableEvent _someEvent;
+
+        public event Action OnEventHappened = delegate { };
+
+
+        private void OnEnable() {
+            _someEvent.AddListener(EventHappened);
+        }
+
+        private void Start() {
+
+            _someEvent.AddListener(EventHappened);
+
+        }
+        private void OnDisable()
+        {
+            _someEvent.RemoveListeners(EventHappened);
+        }
+        private void EventHappened() {
+            OnEventHappened();
+        }
+
+    }
+}
