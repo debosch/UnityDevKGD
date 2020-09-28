1918a3d5685b34c24b67f2627d8f13ad4be8ee7a
Alexandr <navacar@mail.ru>
2020-09-28 20:43:02 +0200

    UpdateManager
    Adding updateManager

---
+++ Assets/Scripts/Managers/UpdateManagers.cs
@@ -0,0 +1,19 @@
+﻿using Events;
+using UnityEngine;
+
+namespace Game {
+    public class UpdateManagers : MonoBehaviour {
+        [SerializeField]
+        private ScriptableEvent _updateEvent;
+        
+        [SerializeField]
+        private ScriptableEvent _fixedEvent;
+        private void Update() {
+            _updateEvent.Dispatch();
+        }
+
+        private void FixedUpdate() {
+            _fixedEvent.Dispatch();
+        }
+    }
+}
