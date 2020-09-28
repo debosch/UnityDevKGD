1b0862438520de65a2dd5463d64135d56e42d84a
Alexandr <navacar@mail.ru>
2020-09-28 20:39:51 +0200

    Events
    Architecture

---
+++ Assets/Scripts/Events/EventDispathcer.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+namespace Events {
+    public class EventDispathcer : MonoBehaviour {
+
+        [SerializeField]
+        private ScriptableEvent _someEvent;
+        public void Dispatch() {
+            _someEvent.Dispatch();
+        }
+        private void update() {
+
+        }
+    }
+}
