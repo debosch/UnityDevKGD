8e71627f77638de82fb7d9d2d75c7f48b05beb10
Valeriy <mr.robariii@gmail.com>
2020-09-28 20:41:06 +0200

    Update Manager Commit

---
+++ Assets/Scripts/Managers/UpdateManager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using Events;
+
+namespace Game {
+
+    public class UpdateManager : MonoBehaviour {
+
+        [SerializeField]
+        private ScriptableEvent _updateEvent;
+
+        [SerializeField]
+        private ScriptableEvent _fixedUpdateEvent;
+
+        private void Update() {
+            _updateEvent.Dispatch();
+        }
+
+        private void FixedUpdate() {
+            _fixedUpdateEvent.Dispatch();
+        }
+    }
+}
+
