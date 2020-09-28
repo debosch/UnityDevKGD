f0b14964ab65dc2b32a0153b350174e87b1456df
BepeckoB <BepeckoB@gmail.com>
2020-09-28 20:43:21 +0200

    Car created

---
+++ Assets/Scripts/Game/Car.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Diagnostics.Tracing;
+using UnityEngine;
+
+namespace Game
+{
+    public class Car : MonoBehaviour
+    {
+        [SerializeField]
+        private EventListener _updateEventListener;
+
+        private void Awake()
+        {
+            _updateEventListener.OnEventHappened += Move;
+        }
+
+        private void Move()
+        {
+            Debug.Log("Move");
+        }
+
+    }
+}
