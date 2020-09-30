b51b63d75c8402b3f45e41a1f99314b66dd55614
Nikita Shpulin <shpulinn@mail.ru>
2020-09-30 20:01:36 +0300

    Car settings and car's assetes added

---
+++ Assets/Scripts/Game/CarSettings.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game {
+
+    [CreateAssetMenu(fileName = "Setting", menuName = "Settings")]
+    public class CarSettings : ScriptableObject {
+
+        [SerializeField]
+        private int _dodgeScore;
+
+
+    }
+}
