8d80ba39321ed418069bd385508832e65e37fcaa
Nikita Shpulin <shpulinn@mail.ru>
2020-09-30 20:36:37 +0300

    CurrentScore asser created

---
+++ Assets/Scripts/Game/CurrentScore.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game {
+    [CreateAssetMenu(fileName = "Current Score", menuName = "Current Score")]
+    public class CurrentScore : ScriptableIntValue {
+
+
+
+    }
+}
