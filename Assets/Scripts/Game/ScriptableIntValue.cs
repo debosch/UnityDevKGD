5219281337337852ed7952f681d9a5311c23e71a
Nikita Shpulin <shpulinn@mail.ru>
2020-09-30 20:47:33 +0300

    CurrentScore asset created (fixed)

--- Assets/Scripts/Game/ScriptableIntValue.cs
+++ Assets/Scripts/Game/ScriptableIntValue.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 namespace Game {
 
+    [CreateAssetMenu(fileName ="Current score", menuName ="Current Score")]
     public class ScriptableIntValue : ScriptableObject {
 
         public int score;
