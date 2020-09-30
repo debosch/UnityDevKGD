45d4c742d201b989b886b6ca2eb8e5680c129a58
Nikita Shpulin <shpulinn@mail.ru>
2020-09-30 20:03:20 +0300

    Car dodge events added

---
+++ Assets/Scripts/Game/CarDodged.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game {
+    [CreateAssetMenu(fileName = "CarDodged", menuName = "CarDodged")]
+    public class CarDodged : ScriptableObject {
+
+
+
+    }
+}
