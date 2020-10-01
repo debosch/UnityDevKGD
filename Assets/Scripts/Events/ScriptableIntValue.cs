266ce79f5feeef82d68dbf2d67946c1008d62e18
Andrey <DrEMartstud@gmail.com>
2020-10-01 18:41:02 +0200

    Created class ScriptableIntValue
    Created class ScriptableIntValue, which has int score in it

---
+++ Assets/Scripts/Events/ScriptableIntValue.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace Game
+{
+
+    [CreateAssetMenu(fileName = "Score", menuName = "Game/Score")]
+    public class ScriptableIntValue : ScriptableObject {
+        public int score;
+    }
+}
