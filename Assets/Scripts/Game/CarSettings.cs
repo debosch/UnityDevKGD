6bd6e56f1b25d184a02679824cb4b07ef9ab5caf
vladislav <vladislavnikwork@gmail.com>
2020-10-01 18:22:36 +0200

    Car settings created

---
+++ Assets/Scripts/Game/CarSettings.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+
+namespace Game {
+
+    [CreateAssetMenu(fileName = "CarSettings", menuName = "CarSettings")]
+    public class CarSettings : ScriptableObject {
+
+        [SerializeField]
+        private int _dodgeScore;
+
+    }
+}
