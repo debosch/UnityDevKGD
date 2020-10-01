22cf7d0b91e78c6a2f42417732b83526f8209030
Valeriy <mr.robariii@gmail.com>
2020-10-01 22:08:53 +0200

    ScriptableIntValue and CarDodged.
    Created ScriptableIntValue and CarDodged event.

---
+++ Assets/Scripts/Game/ScriptableIntValue.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Game {
+
+    [CreateAssetMenu(fileName = "New Int Value", menuName = "Data/Int Value")]
+    public class ScriptableIntValue : ScriptableObject {
+
+        public int score;
+
+    }
+
+}
