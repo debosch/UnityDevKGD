9e1798018cfa9872b9b26428c2c64812bff9f2c7
Nikita <Nkanunov@gmail.com>
2020-10-02 00:51:03 +0200

    Car settings and game cars assets created

---
+++ Assets/Scripts/Game/CarSettings.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+namespace Game {
+    
+    [CreateAssetMenu(fileName = "CarSettings", menuName = "Car/CarSettings")]
+    public class CarSettings : ScriptableObject {
+        public int dodgeScore;
+    }
+}
