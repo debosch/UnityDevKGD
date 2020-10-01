17bba811f28614802250a072b6a7334d7349bf44
Alexandr <navacar@mail.ru>
2020-10-01 10:25:30 +0200

    Car settings Script
    Adding CarSettings

---
+++ Assets/Scripts/Game/CarSettings.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Car setting", menuName = "Car setting")]
+public class CarSettings : ScriptableObject {
+    [SerializeField] 
+    private int _dodgeScore; 
+}
