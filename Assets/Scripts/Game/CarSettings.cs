392202c776ceb88294f0f635621db2dd253eebdd
Evgenii <jekaweek1@gmail.com>
2020-10-01 22:10:23 +0200

    Car Settings commit

---
+++ Assets/Scripts/Game/CarSettings.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Game {
+
+    [CreateAssetMenu(fileName = "NewCar", menuName ="Cars")]
+    public class CarSettings : ScriptableObject {
+
+        public int dodgeScore;
+
+    }
+
+}
