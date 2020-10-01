c8a0ef539f0b38aaaadb255c3d58602313d1bf6f
Evgenii <jekaweek1@gmail.com>
2020-10-01 22:12:28 +0200

    Add ScriptableIntValue and CarDodged

---
+++ Assets/Scripts/Game/ScriptableIntValue.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace Game {
+
+    [CreateAssetMenu(fileName ="New Int Value", menuName ="Data/IntValue")]
+    public class ScriptableIntValue : ScriptableObject {
+
+        public int score;
+
+    }
+}
