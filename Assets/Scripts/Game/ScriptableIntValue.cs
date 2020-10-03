0a66054e969e2fb03412e5a6ff14ff545de44a8f
Anastasia Lutovinova <alutovinovaa@gmail.com>
2020-10-03 21:34:10 +0200

    create ScriptableIntValue

---
+++ Assets/Scripts/Game/ScriptableIntValue.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game {
+
+    [CreateAssetMenu(fileName = "Score", menuName = "IntValue")]
+    public class ScriptableIntValue : ScriptableObject {
+
+        public int score;
+
+    }
+}
