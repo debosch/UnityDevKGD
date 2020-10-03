044d90780a991da580e951315b57b34212a1c5b8
Denis <denis.zhuravkov@gmail.com>
2020-10-03 19:26:28 +0200

    CarSettings class created with assets for 4 different types of vehicle

---
+++ Assets/Scripts/Game/CarSettings.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace Game {
+
+    [CreateAssetMenu(fileName = "CarSettings", menuName = "CarSettings", order = 0)]
+    public class CarSettings : ScriptableObject {
+    
+        [SerializeField]
+        private int _dodgeScore = 0;
+    }
+}
