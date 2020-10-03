8d0f7300c782226ad05cf17742686209e139514d
Anastasia Lutovinova <alutovinovaa@gmail.com>
2020-10-03 16:54:36 +0200

    create Events

---
+++ Assets/Scripts/Events/EventListener.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace Events {
+
+    public class EventListener : MonoBehaviour {
+
+        [SerializeField]
+        private ScriptableEvent _someEvent;
+
+        public event Action OnEventHappend = delegate { };
+
+        private void OnEnable() {
+
+            _someEvent.AddListeners(EventHappened);
+
+        }
+
+	private void OnDisable() {
+            _someEvent.RemoveListener(EventHappened);
+	    
+	}
+
+        private void EventHappened() {
+
+            OnEventHappend.Invoke();
+        }
+
+
+
+
+    }
+}
