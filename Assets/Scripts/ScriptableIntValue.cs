a6344a9b00d14a975b314909ef95f118a107181c
Denis <denis.zhuravkov@gmail.com>
2020-10-03 19:28:20 +0200

    ScriptableIntValue class created

---
+++ Assets/Scripts/ScriptableIntValue.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "ScriptableIntValue", menuName = "ScriptableIntValue")]
+public class ScriptableIntValue : ScriptableObject {
+    
+    [SerializeField]
+    private int value = 0;
+}
