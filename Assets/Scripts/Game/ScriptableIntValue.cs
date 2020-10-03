0459a7ee4fb71be1caa744fb47d3d520420014ef
Alex Osi <noemail>
2020-10-03 17:48:23 +0200

    Created class ScriptableIntValue

---
+++ Assets/Scripts/Game/ScriptableIntValue.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Value {
+
+    [CreateAssetMenu(fileName = "Score", menuName = "Score")]
+    public class ScriptableIntValue : ScriptableObject {
+        public int score = 0;
+    }
+}
