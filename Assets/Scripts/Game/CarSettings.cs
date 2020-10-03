adb4c2748d44317245f94b9076d9ca0baeab414a
Alex Osi <noemail>
2020-10-03 16:57:02 +0200

    Created class CarSettings with car settings and assets

---
+++ Assets/Scripts/Game/CarSettings.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CarSettings {
+
+    [CreateAssetMenu(fileName = "Score", menuName = "CurrentScore")]
+    public class CarSettings : ScriptableObject {
+        [SerializeField]
+        private int _dodgeScore = 0;
+    }
+
+}
