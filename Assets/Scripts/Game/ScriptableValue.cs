615d92c4835c183b3a67fb0ded3844c4597f1375
BepeckoB <BepeckoB@gmail.com>
2020-10-03 23:41:17 +0200

    created SriptableValue class

---
+++ Assets/Scripts/Game/ScriptableValue.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game {
+
+    [CreateAssetMenu(fileName = "Score", menuName = "Score")]
+    public class ScriptableValue : ScriptableObject {
+        public int score;
+    }
+}
