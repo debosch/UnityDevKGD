0f1fbe86ef32004acddff063c2ef56d19943953d
Anastasia Lutovinova <alutovinovaa@gmail.com>
2020-10-03 16:56:27 +0200

    car created

---
+++ Assets/Scripts/Game/Car.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Events;
+
+namespace Game {
+
+
+    public class Car : MonoBehaviour {
+	[SerializeField]
+	private EventListener _updateEventListener;
+
+	private void Awake() {
+	    _updateEventListener.OnEventHappend += Move;
+	}
+
+	private void Move() {
+	    Debug.Log("Move");
+	    
+	}
+    }
+}
