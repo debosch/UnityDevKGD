0bc2625d57a53d66272dccaba82d608011efdcde
Anastasia <nastya_kalinina2000@mail.ru>
2020-10-04 18:35:17 +0200

    CarsSettings SC

---
+++ Assets/Scripts/Cars/CarsSettings.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace CarsSettings {
+    [CreateAssetMenu(fileName = "CarsSettings", menuName = "CarsSettings")]
+    public class CarsSettings : ScriptableObject {
+        [SerializeField]
+        private int _dodgedScore;
+    }
+}
