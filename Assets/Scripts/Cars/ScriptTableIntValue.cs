6aa6044c2c0038e239bc0c201a8e9a6ebb206b7e
Anastasia <nastya_kalinina2000@mail.ru>
2020-10-04 18:39:15 +0200

    scriptTable SC

---
+++ Assets/Scripts/Cars/ScriptTableIntValue.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace ScripTableIntValue {
+    [CreateAssetMenu(fileName = "Score", menuName = "new Score")]
+    public class ScriptTableIntValue : ScriptableObject {
+        [SerializeField]
+        public int Score;
+    }
+}
