e85b73a3295b4f17af3c79e2d35b3878c09d0045
Kvotua <kvotua.job@gmail.com>
2020-10-05 01:06:11 +0200

    CarSettings

---
+++ Assets/Scripts/Game/CarSettings.cs
@@ -0,0 +1,12 @@
+﻿using Events;
+using System;
+using UnityEngine;
+
+namespace Game {
+
+    [CreateAssetMenu(fileName = "Car", menuName = "Car")]
+    public class CarSettings : ScriptableObject {
+        [SerializeField]
+        private UInt64 _dodgeScore;
+    }
+}
