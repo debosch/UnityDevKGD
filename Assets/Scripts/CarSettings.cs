11480b2d1302a9ef0c6b2218c7d1fb004ed18546
BarbadosP <var0720@gmail.com>
2020-10-04 17:20:24 +0200

    Creat  type car

---
+++ Assets/Scripts/CarSettings.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Type Car", menuName = "New Type Car")]
+public class CarSettings : ScriptableObject
+{
+    [SerializeField]
+    private int dodgeScore;
+}
