0b1c5c4c7c96c26c919f3b07d5d2df2bc810d7d5
Kvotua <kvotua.job@gmail.com>
2020-10-05 01:07:56 +0200

    Scriptablevalue Cardodged

---
+++ Assets/Scripts/Game/ScriptableIntValue.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game {
+
+    [CreateAssetMenu(fileName = "Score", menuName = "Score")]
+    public class ScriptableIntValue : ScriptableObject {
+        public int score;
+    }
+}
