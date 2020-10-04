92870d8ce6f71ce582553090a3f8d74f08fc0204
SaraJess <sarycheva.00@mail.ru>
2020-10-04 17:42:15 +0200

    Create event Dodge and Score asset

---
+++ Assets/Scripts/CarDodged.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Dodge", menuName = "New Dodge")]
+public class CarDodged : ScriptableObject
+{
+    [SerializeField]
+    private ScriptableIntValue totalScore;
+    public void Dodge(int dodgescore) {
+        totalScore.score = totalScore.score + dodgescore;
+    }
+}
