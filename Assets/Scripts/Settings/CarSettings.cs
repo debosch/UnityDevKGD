bdbec5900819927aa392844c122cfe163471f4e7
gg <66880r@gmail.com>
2020-10-05 01:16:32 +0200

    сlass CarSettings created

---
+++ Assets/Scripts/Settings/CarSettings.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game {
+
+    [CreateAssetMenu(fileName = "NewCarSettings", menuName = "CarSettings")]
+    public class CarSettings : ScriptableObject {
+
+        [SerializeField]
+        private int _dodgeScore = 0;
+    }
+}
