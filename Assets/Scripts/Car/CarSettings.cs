9879a09d35603e8620319d99ad0549ef4b72de15
Ilya <elya5911@mail.ru>
2020-10-04 16:54:16 +0200

    CarSettings_cs

---
+++ Assets/Scripts/Car/CarSettings.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+namespace CarSettings {
+
+    [CreateAssetMenu(fileName = "CarSettings", menuName = "CarSettings")]
+    public class CarSettings : ScriptableObject {
+
+        [SerializeField]
+        public int dodjeScor = 0;
+    }
+}
