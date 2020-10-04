016bff4344aacb66aa5459c7562cb462f95f0bdb
Alex Silkov <st.jimmy39@mail.ru>
2020-10-04 02:21:16 +0200

    CarSettings class created

---
+++ Assets/Scripts/Game/CarSettings.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game {
+
+    [CreateAssetMenu(fileName ="Car Settings",menuName = "Game/Car Settings")]
+    public class CarSettings : ScriptableObject {
+
+        public int dodgeScore;
+    }
+}
+
