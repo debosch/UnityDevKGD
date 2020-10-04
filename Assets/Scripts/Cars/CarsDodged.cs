d9bb32881fe620c412cf09b93c92c97763a332c9
Anastasia <nastya_kalinina2000@mail.ru>
2020-10-04 18:55:52 +0200

    cars dodged

---
+++ Assets/Scripts/Cars/CarsDodged.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace ScriptTableIntValue {
+    [CreateAssetMenu(fileName = "CarsDodged", menuName = "new CarsDodged")]
+    public class CarsDodje : ScriptableObject {
+        [SerializeField]
+        private ScriptTableIntValue _score;
+
+        public void Dodged(int dodgedScore) {
+            _score.Score += dodgedScore;
+        }
+    }
+}
