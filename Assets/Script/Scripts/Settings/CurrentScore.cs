909839bf874a9d0acde20396c83880cf8b9b10ba
Oleg <oleg.kalyugin2001@mail.ru>
2020-10-04 21:28:09 +0200

    New script CurrentScore

--- Assets/Script/Scripts/Settings/CurrentScore.cs
+++ Assets/Script/Scripts/Settings/CurrentScore.cs
@@ -1,18 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Settings;
 
-public class CurrentScore : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+public class CurrentScore : ScriptableIntValue {
+    public int save_score;
 }
