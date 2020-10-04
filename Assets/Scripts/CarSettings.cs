393dc17ca137d7249400d684f714ec458e97a9b7
Dan <kdanil01@mail.ru>
2020-10-04 19:21:30 +0200

    Created CarSettings and ScriptableIntValue

---
+++ Assets/Scripts/CarSettings.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Type Car", menuName = "New Type Car")]
+
+public class CarSettings : ScriptableObject
+{
+    [SerializeField]
+    private string _name;
+
+    [SerializeField]
+    private int _dodgeScore;
+}
---
+++ Assets/Scripts/ScriptableIntValue.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Score", menuName = "New Score")]
+
+public class ScriptableIntValue : ScriptableObject
+{
+    [SerializeField]
+    public int score;
+}
