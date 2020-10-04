31078c97a6f1ffaf730b2e03d8ca95b606937709
gg <66880r@gmail.com>
2020-10-04 14:35:01 +0200

    event system created

---
+++ Assets/Scripts/Events/EventDispatcher.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace Events {
+
+    public class EventDispatcher : MonoBehaviour {
+
+        [SerializeField]
+        private ScriptableEvent _someEvent;
+
+        public void Dispatch() {
+            _someEvent.Dispatch();
+        }
+    }
+}
