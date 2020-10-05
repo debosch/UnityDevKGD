5f91cf1e1d7771078bfe8c3182176cbd8519b759
VysogotaOC <kamaeff.petya@gmail.com>
2020-10-05 14:10:32 +0200

    ScriptableIntValue script added

---
+++ Assets/Scripts/Game/ScriptableIntValue.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+
+namespace Game {
+    [CreateAssetMenu (fileName = "ScriptableIntValue", menuName = "Car/ScriptableIntValue")]
+    public class ScriptableIntValue : ScriptableObject {
+        public int score;
+    }
+}
