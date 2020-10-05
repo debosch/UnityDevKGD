a98ea52c9b436e4a4ced484197558eeed9abb661
VysogotaOC <kamaeff.petya@gmail.com>
2020-10-05 13:43:25 +0200

    CarSettings script added

---
+++ Assets/Scripts/Game/CarSettings.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+
+namespace Game {
+    [CreateAssetMenu(fileName = "CarSettings", menuName = "Car/CarSettings")]
+    public class CarSettings : ScriptableObject {
+        public int dodgeScore;
+    }
+}
