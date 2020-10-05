264c1bb7633a2b7dfc28e8fe71cfbf0895009a2a
Alexandr <navacar@mail.ru>
2020-10-05 23:30:54 +0200

    Adding UI
    Adding UI manager

---
+++ Assets/Scripts/UI/UIManager.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+namespace UI {
+    public class UIManager : MonoBehaviour {
+
+        public static UIManager Instance;
+
+        [SerializeField]
+        private Fader _fader;
+
+        private string _currentSceneName = "GamePlay";
+
+        private void Awake() {
+            if (Instance != null) {
+                Destroy(gameObject);
+                return;
+            }
+
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+
+        private void Start() {
+            _fader.OnFadeIn += OnSceneFadeIn;
+            _fader.FadeIn();
+        }
+
+        private void OnSceneFadeIn() {
+            StartCoroutine(FadeOutAndLoadGameplay());
+        }
+
+        private IEnumerator FadeOutAndLoadGameplay() {
+            yield return new WaitForSeconds(3f);
+
+            _fader.OnFadeOut += LoadGameplayScene;
+            _fader.FadeOut();
+        }
+
+        private void LoadGameplayScene() {
+            _fader.OnFadeOut -= LoadGameplayScene;
+            SceneManager.LoadScene("GamePlay");
+            _currentSceneName = _currentSceneName == "GamePlay" ? "Menu" : "GamePlay";
+        }
+
+        private IEnumerator LoadGameplaySceneCoroutine(string sceneName) {
+            var asincOperation  = SceneManager.LoadSceneAsync(sceneName);
+            while (!asincOperation.isDone) {
+                yield return null;
+            }
+
+            yield return new WaitForSeconds(3f);
+            _fader.FadeIn();
+        }
+
+    }
+}
