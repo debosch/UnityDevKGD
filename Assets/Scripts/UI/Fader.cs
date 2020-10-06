5c0468579b52ede42c9523aae4fb080baeeff72d
Kvotua <kvotua.job@gmail.com>
2020-10-06 02:54:14 +0200

    Adding Fader

---
+++ Assets/Scripts/UI/Fader.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace UI {
+    public class Fader : MonoBehaviour {
+
+        [SerializeField] private CanvasGroup _canvasGroup;
+        [SerializeField] private float _fadeTime;
+
+        public event Action OnFadeIn = delegate { };
+        public event Action OnFadeOut = delegate { };
+
+        public void FadeIn() {
+
+            StartCoroutine(FadeInCoroutine());
+
+        }
+        public void FadeOut() {
+
+            StartCoroutine(FadeOutCoroutine());
+        }
+
+        private IEnumerator FadeInCoroutine() {
+            yield return StartCoroutine(FadeCoroutine(1f, 0f));
+            OnFadeIn();
+        }
+
+
+        private IEnumerator FadeOutCoroutine() {
+            yield return StartCoroutine(FadeCoroutine(0f, 1f));
+            OnFadeOut();
+        }
+
+
+        private IEnumerator FadeCoroutine(float fromAlpha,float targetAlphe) {
+            var timer = 0f;
+            _canvasGroup.alpha = fromAlpha;
+            while (timer < _fadeTime) {
+                timer += Time.deltaTime;
+                _canvasGroup.alpha = Mathf.Lerp(_canvasGroup.alpha, targetAlphe, timer / _fadeTime);
+                _canvasGroup.interactable = _canvasGroup.alpha > 0f;
+                yield return null;
+            }
+        }
+    }
+}
