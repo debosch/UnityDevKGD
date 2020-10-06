33f9725de5d42c63af58983860d44cf9880ac076
Rustam Mamedov <mamedov@panzerdog.com>
2020-10-06 08:01:00 +0200

    UIManager

---
+++ Assets/Scripts/UI/UIManager.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace UI {
+
+    public class UIManager : MonoBehaviour {
+
+        public static UIManager Instance;
+
+        [SerializeField]
+        private Fader _fader;
+
+        private string _currentSceneName = "Gameplay";
+
+        private void Awake() {
+            if (Instance != null) {
+                Destroy(gameObject);
+                return;
+            }
+
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+
+        private void Start() {
+            _fader.OnFadeIn += OnSceneFadeIn;
+            _fader.FadeIn();
+        }
+
+        private void OnSceneFadeIn() {
+            StartCoroutine(FadeOutAndLoadGameplay());
+        }
+
+        private IEnumerator FadeOutAndLoadGameplay() {
+            yield return new WaitForSeconds(3f);
+
+            _fader.OnFadeOut += LoadGameplayScene;
+            _fader.FadeOut();
+        }
+
+        private void LoadGameplayScene() {
+            _fader.OnFadeOut -= LoadGameplayScene;
+            StartCoroutine(LoadSceneCoroutine(_currentSceneName));
+            _currentSceneName = _currentSceneName == "Gameplay" ? "Menu" : "Gameplay";
+        }
+
+        private IEnumerator LoadSceneCoroutine(string sceneName) {
+            var asyncOperation = SceneManager.LoadSceneAsync(sceneName);
+            while (!asyncOperation.isDone) {
+                yield return null;
+            }
+
+            yield return new WaitForSeconds(3f);
+
+            _fader.FadeIn();
+        }
+
+    }
+}
