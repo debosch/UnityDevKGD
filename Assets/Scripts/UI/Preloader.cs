3a3fa10c07b2430ef2dc428459214571b1f7ac44
Rustam Mamedov <mamedov@panzerdog.com>
2020-10-06 08:00:11 +0200

    preloader

---
+++ Assets/Scripts/UI/Preloader.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace UI {
+
+    public class Preloader : MonoBehaviour {
+
+        private void Start() {
+            StartCoroutine(LoadMenuScene());
+        }
+
+        private IEnumerator LoadMenuScene() {
+            var asyncOperation = SceneManager.LoadSceneAsync("Menu");
+            while (!asyncOperation.isDone) {
+                Debug.Log(asyncOperation.progress);
+                yield return null;
+            }
+        }
+    }
+}
