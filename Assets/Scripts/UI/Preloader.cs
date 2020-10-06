7ee1ae4ad22a6a35b5d8aba64f4373eb2d220cda
Kvotua <kvotua.job@gmail.com>
2020-10-06 02:54:30 +0200

    Adding Preloader

---
+++ Assets/Scripts/UI/Preloader.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+namespace UI {
+
+
+    public class Preloader : MonoBehaviour {
+
+        private void Start() {
+           StartCoroutine(LoadMenuScene());
+        }
+
+        private IEnumerator LoadMenuScene() {
+            var asyncOperation = SceneManager.LoadSceneAsync("Menu");
+            while (!asyncOperation.isDone) {
+                Debug.Log(asyncOperation.progress);
+                yield return null;
+            }
+        }
+
+    }
+}
