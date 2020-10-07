7d5db3141bdec117cad40f62a8ae498930c80bee
Vlad <vlad.shiltsin@gmail.com>
2020-10-07 14:54:49 +0200

    Fader

---
+++ Assets/Scripts/UI/Fader.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+namespace UI {
+
+    public class Fader : MonoBehaviour {
+
+        [SerializeField]
+        private CanvasGroup _canvasGroup;
+
+        [SerializeField]
+        private float _fadeTime;
+
+        public Action OnFadeIn = delegate { };
+        public Action OnFadeOut = delegate { };
+
+        public void FadeIn() {
+            StartCoroutine(FadeInCoroutine());
+        }
+
+        public void FadeOut() {
+            StartCoroutine(FadeOutCoroutine());
+        }
+
+        private IEnumerator FadeInCoroutine() {
+            yield return StartCoroutine(FadeCoroutine(1f, 0f));
+            OnFadeIn();
+        }
+
+        private IEnumerator FadeOutCoroutine() {
+            yield return StartCoroutine(FadeCoroutine(0f, 1f));
+            OnFadeOut();
+        }
+
+        private IEnumerator FadeCoroutine(float fromAlpha, float targetAlpha) {
+            float timer = 0f;
+            _canvasGroup.alpha = fromAlpha;
+
+            while (timer < _fadeTime) {
+                timer += Time.deltaTime;
+                _canvasGroup.alpha = Mathf.Lerp(_canvasGroup.alpha, targetAlpha, timer / _fadeTime);
+                _canvasGroup.interactable = _canvasGroup.alpha > 0f;
+                yield return null;
+            }
+        }
+
+    }
+}
